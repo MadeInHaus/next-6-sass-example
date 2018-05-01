2555efa675711aff9eb501cf6d63491dd0e89c72
Claus Wahlers <claus@codeazur.com.br>
2018-05-01 17:23:17 -0300

    custom ExtractTextPlugin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 const withSass = require('@zeit/next-sass');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extract = new ExtractTextPlugin({ filename: 'static/style.css' });
+
 module.exports = withSass({
     cssModules: true,
+    extractCSSPlugin: extract,
+    webpack: config => {
+        config.plugins.push(extract);
+        return config;
+    },
 });
