352a132a863cfc8e342123648cf627a1335e0fa6
Claus Wahlers <claus@codeazur.com.br>
2018-05-01 17:33:07 -0300

    have ExtractTextPlugin create one css per bundle

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withSass = require('@zeit/next-sass');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extract = new ExtractTextPlugin({ filename: 'static/style.css' });
+const extract = new ExtractTextPlugin({ filename: 'static/[contenthash].css' });
 
 module.exports = withSass({
     cssModules: true,
--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,20 @@ import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
     render() {
+        const { buildManifest } = this.props;
+        const { css } = buildManifest;
         return (
             <html>
                 <Head>
-                    <link rel="stylesheet" href="/_next/static/style.css" />
+                    {css.map(file => {
+                        return (
+                            <link
+                                rel="stylesheet"
+                                href={`/_next/${file}`}
+                                key={file}
+                            />
+                        );
+                    })}
                 </Head>
                 <body>
                     <Main />
